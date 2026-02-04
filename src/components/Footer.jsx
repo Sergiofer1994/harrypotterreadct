60f6a398054c216d49886b7a7316c85387ea34a7
Sergiofer1994 <sergiofer1994@gmail.com>
2026-02-04 17:07:50 +0100

    feat: footer.jsx  created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+<footer class="footerHarryPother">
+  <div class="footerContent">
+    <p>&copy; 2024 Harry Pother. All rights reserved.</p>
+    <p>
+      Información obtenida de <a href="https://hp-api.onrender.com">aquí</a>.
+    </p>
+  </div>
+</footer>
+
