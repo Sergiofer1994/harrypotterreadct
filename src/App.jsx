9f86eb233ee8c01a49b6061840b614de90ea6c95
Sergiofer1994 <sergiofer1994@gmail.com>
2026-02-05 12:57:08 +0100

    feat: footer created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './app.css'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+ function App() {
+
+  return (
+    <>
+      <Header/>
+      
+      <Footer/>
+    </>
+  )
+}
+export default App
