6ac85d54c732b9c3813c2fce11edcd9bf4b9ae4a
Sergiofer1994 <sergiofer1994@gmail.com>
2026-02-06 19:32:55 +0100

    feat: header and footer CSS done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './app.css'
 import Header from './components/Header/Header.jsx'
-
-
+import Footer from './components/footer/Footer.jsx'
 function App() {
   return (
     <>
+      <Footer/>
       <Header/>
     </>
   )
 }
+
 
 export default App
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,17 @@
+import React from 'react';
+import './Footer.css';
 function Footer() {
   return (
-    <footer className="footerHarryPother">
+    <footer className="footerHarryPotter">
       <div className="footerContent">
         <p>&copy; 2024 Harry Pother. All rights reserved.</p>
         <p>
-          Información obtenida de <a href="https://hp-api.onrender.com">aquí</a>.
+          Información obtenida de
+          <a href="https://hp-api.onrender.com">aquí</a>.
         </p>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
